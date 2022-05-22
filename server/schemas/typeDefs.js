b24cfb7207ddead89ff393211924bc9363722df3
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-05-21 21:47:17 -0500

    created type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,37 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type 
+    
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+}
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+}
+type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
 `;
 
 // export the typeDefs
