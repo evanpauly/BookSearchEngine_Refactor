81b059c038c650ca49909c82b2dc36827aa88aed
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-05-22 20:51:52 -0500

    still trying to fix errors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -11,21 +10,6 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
-
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
@@ -33,6 +17,9 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
